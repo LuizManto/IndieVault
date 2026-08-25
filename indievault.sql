a5f1c7e29b76dad5aaeeb5c261e885f960116d8b
Luiz Mantovani <luizclaudioamantovani@gmail.com>
2026-08-24 23:39:36 -0300

    DataBank

---
+++ indievault.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 24/05/2026 às 03:21
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `indievault`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `comentarios`
+--
+-- Erro ao ler a estrutura para a tabela indievault.comentarios: #1932 - Table 'indievault.comentarios' doesn't exist in engine
+-- Erro ao ler dados para tabela indievault.comentarios: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `indievault`.`comentarios`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `favoritos`
+--
+-- Erro ao ler a estrutura para a tabela indievault.favoritos: #1932 - Table 'indievault.favoritos' doesn't exist in engine
+-- Erro ao ler dados para tabela indievault.favoritos: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `indievault`.`favoritos`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `jogos_indie`
+--
+-- Erro ao ler a estrutura para a tabela indievault.jogos_indie: #1932 - Table 'indievault.jogos_indie' doesn't exist in engine
+-- Erro ao ler dados para tabela indievault.jogos_indie: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `indievault`.`jogos_indie`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+-- Erro ao ler a estrutura para a tabela indievault.usuarios: #1932 - Table 'indievault.usuarios' doesn't exist in engine
+-- Erro ao ler dados para tabela indievault.usuarios: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `indievault`.`usuarios`' na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
